33b99b9b6f98740d7050252266f0124ad66e14cf
nerrad1776 <darrenalanshingler@gmail.com>
2023-05-01 11:45:22 -0400

    Ice Boots Now Available under ingredients

--- src/main/java/fam/shingler/iceboots/item/ModItems.java
+++ src/main/java/fam/shingler/iceboots/item/ModItems.java
@@ -1,18 +1,25 @@
 package fam.shingler.iceboots.item;
 
 import fam.shingler.iceboots.IceBoots;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+
+
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, IceBoots.MOD_ID);
 
     public static final RegistryObject<Item> ICEBOOTS = ITEMS.register("iceboots",
-            () -> new Item(new Item.Properties()));
+            () -> new IceBootsItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS,
+                    new Item.Properties().stacksTo(1)));
+
+
 
     public static void register(IEventBus eventBus)
     {
